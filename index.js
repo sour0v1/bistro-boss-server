5b452909ce46ffff8c3b8cee049718a87551cf83
Sourov <sourovdas730@gmail.com>
2024-05-31 13:41:36 +0600

    created delete user api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -32,6 +32,7 @@ async function run() {
         const menuCollection = database.collection("menu");
         const reviewsCollection = database.collection("reviews");
         const menuItemCollection = database.collection('itemCarts');
+        const userCollection = database.collection('users');
 
         app.get('/menu', async(req, res) =>{
             const result = await menuCollection.find().toArray();
@@ -48,17 +49,41 @@ async function run() {
             res.send(result)
             // console.log(category);
         })
+        app.get('/users', async(req, res) =>{
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
         app.get('/carts', async(req, res) =>{
             const userEmail = req.query.email;
-            console.log(userEmail)
+            // console.log(userEmail)
             const query = {email : userEmail}
             const result = await menuItemCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.delete('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            // console.log(id)
+            const query = {_id : new ObjectId(id)}
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
         app.post('/cart', async(req, res) =>{
             const item = req.body;
             // console.log(item);
             const result = await menuItemCollection.insertOne(item);
+            res.send(result);
+        })
+        app.post('/users', async(req, res) =>{
+            const userInfo = req.body;
+            // console.log(item);
+            // insert user info if user doesn't exist
+            // you can do this in many way 1.unique email, 2.upsert, 3. simple checking
+            const query = {email : userInfo.email}
+            const existingUser = await userCollection.findOne(query);
+            if(existingUser){
+                return res.send({message : 'user already exist', insertedId : null})
+            }
+            const result = await userCollection.insertOne(userInfo);
             res.send(result);
         })
 
